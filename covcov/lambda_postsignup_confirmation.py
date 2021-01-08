74f32145afc5b7457b7ef234c394a676edeb87f8
haa <wharouny.tp@gmail.com>
2021-01-08 21:48:50 +0100

    fix bug - lambda_postsignup_confirmation.py

--- covcov/lambda_postsignup_confirmation.py
+++ covcov/lambda_postsignup_confirmation.py
@@ -1,8 +1,8 @@
 from covcov.infrastructure.db.database import Database
-from covcov.application import misc_utils as create_user
+from covcov.application import misc_utils as util
 
 db = Database("database")
 
 def handle(event, context):
-  create_user(event, db)
+  util.create_user(event, db)
   return event
