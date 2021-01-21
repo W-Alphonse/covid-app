e40165c12354f914f0ff04d132df83947c0085c3
haa <wharouny.tp@gmail.com>
2021-01-21 02:21:33 +0100

    evol - 1st commit SAM

--- sam-all.sh
+++ sam-all.sh
@@ -1,7 +1,8 @@
 sam build && 
-sam package --s3-bucket covcov --output-template-file out.yml &&
+sam package --output-template-file out.yml &&
 sam deploy --no-confirm-changeset --template-file ./out.yml
 
 # sam package --s3-bucket covcov --output-template-file out.yml &&
 # sam deploy --no-confirm-changeset --template-file ./out.yml --stack-name test-2
 
+#https://stackoverflow.com/questions/62536095/how-to-set-a-stage-name-in-a-sam-template
