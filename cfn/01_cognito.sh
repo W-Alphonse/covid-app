6d908c9509b3d1b93ed1ce6d48336e8af13f1849
haa <wharouny.tp@gmail.com>
2021-01-25 15:18:08 +0100

    evol - Cloudformation configs + scripts

--- cfn/01_cognito.sh
+++ cfn/01_cognito.sh
@@ -1,10 +1,9 @@
 ARTIFACT_PFIX="01_cognito"
-echo $ARTIFACT_PFIX
-echo $ARTIFACT_PFIX-out.yml
+STACK_NAME="covcov-cognito-DEV"
 
 sam build   -t $ARTIFACT_PFIX-template.yaml --config-file $ARTIFACT_PFIX-samconfig.toml &&
 sam package -t $ARTIFACT_PFIX-template.yaml --config-file $ARTIFACT_PFIX-samconfig.toml --output-template-file $ARTIFACT_PFIX-out.yml &&
-sam deploy  -t $ARTIFACT_PFIX-template.yaml --config-file $ARTIFACT_PFIX-samconfig.toml --template-file ./$ARTIFACT_PFIX-out.yml
+sam deploy  -t $ARTIFACT_PFIX-template.yaml --config-file $ARTIFACT_PFIX-samconfig.toml --template-file ./$ARTIFACT_PFIX-out.yml  --stack-name $STACK_NAME
 
 # sam package --s3-bucket covcov --output-template-file out.yml &&
 # sam deploy --no-confirm-changeset --template-file ./out.yml --stack-name test-2
