e7d342d9f340c7ab5a0f7294839189f8520c365d
haa <wharouny.tp@gmail.com>
2021-01-30 01:58:43 +0100

    evol - variablize sam configuration file

--- cfn/01_cognito.sh
+++ cfn/01_cognito.sh
@@ -1,2 +1,13 @@
-. common.sh '01_cognito' 'covcov-cognito-DEV'
+
+export ARTIFACT_PFIX=01_cognito
+export STACK_NAME=covcov-cognito
+export ENV=$1
+
+echo ENV $ENV
+
+. sam-prepare-config.sh  $ARTIFACT_PFIX $ENV
+#. sam-execute-cmds.sh $ARTIFACT_PFIX $STACK_NAME-$ENV
+
+
+#. sam-execute-cmds.sh '01_cognito' 'covcov-cognito-DEV'
 
