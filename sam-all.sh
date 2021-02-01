5185c51fa52766f304a4f6f61cb47a5b89af4a4d
haa <wharouny.tp@gmail.com>
2021-02-01 23:13:07 +0100

    evol - SAM and Cloudformation

--- sam-all.sh
+++
@@ -1,8 +0,0 @@
-sam build && 
-sam package --output-template-file out.yml &&
-sam deploy --no-confirm-changeset --template-file ./out.yml
-
-# sam package --s3-bucket covcov --output-template-file out.yml &&
-# sam deploy --no-confirm-changeset --template-file ./out.yml --stack-name test-2
-
-#https://stackoverflow.com/questions/62536095/how-to-set-a-stage-name-in-a-sam-template
