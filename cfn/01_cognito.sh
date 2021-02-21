05eb6f33e04d6abd273c20b21d06f02f3c96d95a
haa <wharouny.tp@gmail.com>
2021-02-21 22:40:46 +0100

    evol - Integrate the SAM KMS deployment

--- cfn/01_cognito.sh
+++ cfn/01_cognito.sh
@@ -1,6 +1,6 @@
 export ARTIFACT_PFIX=01_cognito
 export STACK_NAME=covcov-cognit
-export PRJ_NAME=covcovio
+export PRJ_NAME=covcovio # PRJ_NAME gives uniques "name" for resources requiring unicity all over AWS. Ex: S3 Bucket, Cognito Domain Name
 export TGET_ENV=$1
 
 . sam-prepare-config.sh
